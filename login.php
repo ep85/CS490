7cedbcaa57884806c34fdc1839ae2b5dfee1ebeb
ep85 <ep85@njit.edu>
2017-02-13 09:42:03 -0500

    update

--- login.php
+++ login.php
@@ -8,21 +8,23 @@
     session_start();
     $success= $_SESSION['success'] || true;
 ?>  
-<form action="process-login.php" method="post" id="loginForm">
-  ucid:<br>
-  <input type="text" name="ucid" value="" >
-  <br>
-  password:<br>
-  <input type="password" name="password" value="" >
-  <br><br>
-  <input type="submit" value="Submit">
-   <?php
-        if($success === false){
-            echo '<p id="failedTXT"> Failed to Authenticate user </p>';
-            $_SESSION['success']=true;
-        }
-    ?>
-</form> 
+<div id="formid">
+    <form action="process-login.php" method="post" id="loginForm">
+      ucid:<br>
+      <input type="text" name="ucid" value="" >
+      <br>
+      password:<br>
+      <input type="password" name="password" value="" >
+      <br><br>
+      <input type="submit" value="Submit">
+       <?php
+            if($success === false){
+                echo '<p id="failedTXT"> Failed to Authenticate user </p>';
+                $_SESSION['success']=true;
+            }
+        ?>
+    </form> 
+</div>
 
 
 
