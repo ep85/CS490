e0998bec17363419e490774f879bdb88d245998f
Nishant Patel <thenishantpatel@gmail.com>
2017-03-05 20:14:59 -0500

    args passed
    args to java program

--- crap/run.php
+++ crap/run.php
@@ -3,54 +3,52 @@
 	ini_set('display_errors', 1);
 	error_reporting(-1);
 
-$fp = fopen('runthis.txt', 'r+');
-fwrite($fp, 'Cats chase mice');
-fclose($fp);
+// $fp = fopen('runthis.txt', 'r+');
+// fwrite($fp, 'Cats chase mice');
+// fclose($fp);
+
+// $post="public static void sayHi(String name1)
+//     {
+//         System.out.println(\"I want to say hi to " + name1 + "!");
+//     }";
 
 // write this to another file and see if you can compile and run that file
-$textcase1= "class sayHi{ 
-	 public static void main(String[] args) {
-        // test cases
-        String name1 = "nish";
-        String name2 = "eric";
-        String name3 = "gus";
+// $textcase1= "class sayHi{ 
+// 	 public static void main(String[] args) {
+//         // test cases
+//         String name1 = "nish";
+//         String name2 = "eric";
+//         String name3 = "gus";
         
-        sayHi(name1);
-        sayHi(name2);
-        sayHi(name3);
-    }
-   public static void sayHi(String name1)
-    { " 
+//         sayHi(name1);
+//         sayHi(name2);
+//         sayHi(name3);
+//     }
+//    " 
 
-    . $post. "}}"
+//     . $post . '}';
+
+	$testcases = [1 => 'nishant', 2 => 'ricP', 3 => 'gusboom'];
 
 
 
+	/* THIS BLOCK works */
 	// Simple compile of .java
 	exec('javac hello.java');
+	
+	$out=shell_exec("java hello $testcases[1]");
+	echo "<pre>$out</pre>";
+	
+	$out=shell_exec("java hello $testcases[2]");
+	echo "<pre>$out</pre>";
+	
+	$out=shell_exec("java hello $testcases[3]");
+	echo "<pre>$out</pre>";
+
 
 	// Attempting to get .java to run
-	exec('java sayHi', $output);
-	print_r($output);
+	// exec('java hello', $output);
+	// print_r($output);
 
-	$hi = "hey";
-	$hi2 = "please work";
-	
-
-
-	// for ($x = 0; $x < 4; $x++) 
-	// {
- //    print "$output[x]";
-	// }
-
-
-	// if ($output[1] == $hi2) 
-	// {
-	// 	echo "this works";
-	// } 
-
- 
-
-	echo "COME ON WORK!!!!!\n";
 	
 ?>
